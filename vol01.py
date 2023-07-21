c5259a3c9c89c37e53db96b877b8642ea741cf2b
daveGitter <daveGitter@users.noreply.github.com>
2023-07-21 02:36:50 +0000

    added prob01

--- vol01.py
+++ vol01.py
@@ -1,7 +1,24 @@
-""" Problem 1
+""" This volume is for the problems from 1 to 50 """
 
-If we list all the natural numbers below 10 that are multiples of 3 or 5,
-we get 3, 5, 6 and 9. The sum of these multiples is 23.
+def prob01(top: int = 10, div: list[int] = [3, 5]) -> int:
+    """ Problem 1
 
-Find the sum of all the multiples of 3 or 5 below 1000.
-"""
+    If we list all the natural numbers below 10 that are multiples of 3 or 5,
+    we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    Find the sum of all the multiples of 3 or 5 below 1000.
+
+    # Note: suppose div always has 2 elements.
+    """
+    sum = 0
+    lcm = div[0] * div[1]  # Least common multiple
+
+    for d in div:
+        d_len = (top - 1) // d  # NOT inclusive
+
+        sum += int(d * (1 + d_len) * d_len / 2)
+    
+    lcm_len = (top - 1) // lcm
+    return sum - int(lcm * (1 + lcm_len) * lcm_len / 2)
+
+print(prob01(1000))
